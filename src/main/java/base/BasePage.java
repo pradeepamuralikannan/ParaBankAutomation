caa2405b5ed88e1d46c29a48b394450393b4dcd8
pradeepamuralikannan <pradeepamuralikannan@gmail.com>
2026-03-28 14:22:02 +0530

    base class created

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,5 +1,45 @@
 package base;
 
-public class BasePage {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//BasePage class—a core part of the Page Object Model (POM). It’s designed to avoid duplicate code and provide reusable methods for all your page classes.
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    //constructor
+    public BasePage(WebDriver driver) {
+        this.driver = driver;   // Assigns driver from test class
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));  // Initializes WebDriverWait (15 sec timeout)
+        PageFactory.initElements(driver, this);   // Initializes all @FindBy elements in child classes
+    }
+    
+  //Waits until element is clickable. prevents ElementNotInteractableException and ElementClickInterceptedException
+    protected void click(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click(); 
+    }
+
+    protected void type(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element)).clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element)).getText();
+    }
+    
+    protected boolean isDisplayed(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+    }
+
+    protected void waitForElement(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
 }
--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -1,5 +1,63 @@
 package base;
 
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+//This Driver Factory class is used to manage WebDriver instances efficiently - especially for Parallel execution
 public class DriverFactory {
 
+	/*If we use Only one driver instance shared across all tests (WebDriver driver = new ChromeDriver())
+	 * Then, If tests run in parallel:
+                Sessions clash
+                Tests interfere with each other
+                Random failures happen
+                
+     * ThreadLocal creates a separate copy of WebDriver for each thread. Instead of one shared driver, each test thread gets its own independent driver instance.
+               ✔ No interference
+               ✔ Safe parallel execution
+               ✔ Better scalability
+	 */
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();   //Creates a separate copy of WebDriver for each thread
+
+    public static WebDriver getDriver() {         // Returns the current thread’s WebDriver
+        return driver.get();
+    }
+
+    public static void initDriver(String browser) {
+
+        switch (browser.toLowerCase()) {
+
+            case "chrome":
+                WebDriverManager.chromedriver().setup();     // Uses WebDriverManager to download/setup ChromeDriver automatically
+                driver.set(new ChromeDriver());
+                break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver.set(new FirefoxDriver());
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver.set(new EdgeDriver());
+                break;
+
+            default:
+                throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+
+        getDriver().manage().window().maximize();
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();         // removes thread-specific value to avoid memory leaks
+        }
+    }
 }
